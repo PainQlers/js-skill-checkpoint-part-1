26b83ab73a72cbfd35a6ec1d2adf9e932e364a49
PainQlers <167845645+PainQlers@users.noreply.github.com>
2025-07-26 13:53:56 +0700

    feat: solve answer ex-3

--- question-3.js
+++ question-3.js
@@ -1,13 +1,15 @@
 // Question #3
-let userPassword = "ssswnalWadqQ";
+let userPassword = "abcdefghijk";
 // เริ่มเขียนโค้ดตรงนี้
+let strongPassword = "aaaaaaaaaa";
+let mediumPassword = "aaaaaa"
 function checkPasswordStrength (userPassword) {
-    if (userPassword > "aaaaaaaaaa") {
+    if (userPassword.length > strongPassword.length) {
         return "Strong"
-    } else if (userPassword >= "aaaaaa" && userPassword <= "aaaaaaaaaa") {
+    } else if (userPassword.length >= mediumPassword.length && userPassword.length <= strongPassword.length) {
         return "Medium"
-    } else if (userPassword < "aaaaaa") {
-        return Weak
+    } else if (userPassword.length < mediumPassword.length) {
+        return "Weak"
     }
 }
 
